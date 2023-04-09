d097eaf04dfe0e0a555dafea82c9234a8f108de8
Aadya Srivastava <aadya28.srivastava@gmail.com>
2023-04-09 14:17:04 +0530

    Modified

--- JavaSubstringComparison.java
+++ JavaSubstringComparison.java
@@ -2,18 +2,19 @@ import java.util.*;
 
 public class JavaSubstringComparison {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter string ");
-        String s = sc.nextLine();
-        System.out.println("Enter the length");
-        int k = sc.nextInt();
-        int n = s.length();
-        int len =  (n-k)+1;
-        String[] strArray = new String[len];
-        for(int i = 0;i<len;i++){
-            strArray[i]=s.substring(i,i+k);
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter string ");
+            String s = sc.nextLine();
+            System.out.println("Enter the length");
+            int k = sc.nextInt();
+            int n = s.length();
+            int len =  (n-k)+1;
+            String[] strArray = new String[len];
+            for(int i = 0;i<len;i++){
+                strArray[i]=s.substring(i,i+k);
+            }
+            Arrays.sort(strArray);
+            System.out.println(strArray[0]+"\n"+strArray[len-1]);
         }
-        Arrays.sort(strArray);
-        System.out.println(strArray[0]+"\n"+strArray[len-1]);
     }
 }
