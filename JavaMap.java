7aa1028f0cad876c90916c461dc96f8bf2dfc495
Aadya Srivastava <aadya28.srivastava@gmail.com>
2023-05-10 14:18:41 +0530

    Program to solve the HackerRank Problem Java Map.

--- JavaMap.java
+++ JavaMap.java
@@ -1,31 +1,29 @@
-//First attempt at solving the HackerRank Problem Java Map.
+//Program to solve the HackerRank Problem Java Map.
 import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
 import java.io.*;
 
 public class JavaMap {
-    public static void main(String[] args)
-    {
-        Scanner scan = new Scanner(System.in);
-        int numOfEntries = scan.nextInt();
-        Map<String,Integer> phoneNumbers = new HashMap<>();
-        for(int i = 0; i < numOfEntries; i++)
-        {
-            String name = scan.nextLine();
-            int phoneNumber = scan.nextInt();
-            phoneNumbers.put(name, phoneNumber);
-            scan.nextLine(); // consume the newline character
+    public static void main(String [] args) throws Exception {
+        /* Read input and save as entries in a HashMap */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        HashMap<String, Integer> map = new HashMap<>();
+        while (n-- > 0) {
+            String name = br.readLine();
+            int phone   = Integer.parseInt(br.readLine());
+            map.put(name, phone);
         }
-        while(scan.hasNext())
-        {
-            String str = scan.nextLine();
-            Integer phoneNumber = phoneNumbers.get(str);
-            if (phoneNumber != null) {
-                System.out.println(phoneNumber);
+
+        /* Read each query and check if it is in our HashMap */
+        String s;
+        while((s = br.readLine()) != null) {
+            if (map.containsKey(s)) {
+                System.out.println(s + "=" + map.get(s));
             } else {
                 System.out.println("Not found");
             }
         }
+
+        br.close();
     }
 }
