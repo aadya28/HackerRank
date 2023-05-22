afb6e89526e3db84ea138f73e040130ba80caadc
Aadya Srivastava <aadya28.srivastava@gmail.com>
2023-05-22 15:58:55 +0530

    Program to solve HackerRank Problem Java Interfaces.

--- JavaInterface.java
+++ JavaInterface.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class JavaInterface {
+//Program to solve HackerRank Problem Java Interfaces.
+import java.util.*;
+interface AdvancedArithmetic{
+    int divisor_sum(int n);
 }
+
+class MyCalculator implements AdvancedArithmetic{
+    public int divisor_sum(int n){
+        int sum = 0;
+        for (int i = 1 ; i <= n/2 ;i++){
+            if (n % i == 0)
+                sum += i;
+            System.out.println(i);
+        }
+        return sum+n;
+    }
+}
+
+class JavaInterface{
+    public static void main(String []args){
+        MyCalculator my_calculator = new MyCalculator();
+        System.out.print("I implemented: ");
+        ImplementedInterfaceNames(my_calculator);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(my_calculator.divisor_sum(n) + "\n");
+        sc.close();
+    }
+
+    //ImplementedInterfaceNames method takes an object and prints the name of the interfaces it implemented
+
+    static void ImplementedInterfaceNames(Object o){
+        Class[] theInterfaces = o.getClass().getInterfaces();
+        for (int i = 0; i < theInterfaces.length; i++){
+            String interfaceName = theInterfaces[i].getName();
+            System.out.println(interfaceName);
+        }
+    }
+}
+
